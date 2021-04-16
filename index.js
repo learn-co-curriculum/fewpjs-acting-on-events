ffada7b5917242827d8247c4b2bf50e067e71522
musiccity365 <m_sackstein@yahoo.com>
2021-04-15 21:01:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Your code here
+let dodger = document.getElementById('dodger');
+
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace('px', '');
+    let left = parseInt(leftNumbers, 10)
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`
+    }
+}
+
+function moveDodgerRight() {
+    let leftNumbers = dodger.style.left.replace('px', '');
+    let left = parseInt(leftNumbers, 10)
+    if (left > 0) {
+        dodger.style.left = `${left + 1}px`
+    }
+}
+
+document.addEventListener('keydown', function(e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft()
+    }
+})
+
+document.addEventListener('keydown', function(e) {
+    if (e.key === "ArrowRight") {
+        moveDodgerRight()
+    }
+})
